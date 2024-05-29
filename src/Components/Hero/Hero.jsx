3db50ce8e8a3b26e35d190b0e53f3a4dc7fc4cf1
kumar Devashish <kumardevashish000@gmail.com>
2024-05-29 13:35:50 +0530

    feat: Add MyWork component to App.jsx and implement MyWork section styling

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Hero.css';
 import profile_img from '../../assets/profile_images.png';
-
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
       <img src={profile_img} alt="Profile" width={100}/>
       <h1><span>I'm Kumar Devashish,</span> blockchain developer based in India.</h1>
       <p>I am a blockchain developer from Bihar, India with 3 years of experience in multiple companies like Microsoft, Google, and Amazon.</p>
       <div className='hero-action'>
-        <div className='hero-connect'>Connect with me</div>
+      <div className='hero-connect'>
+          <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -6,7 +6,7 @@ import arrow_icon from '../../assets/arrow_icon.svg'
 import mywork_data from '../../assets/mywork_data';
 const MyWork = () => {
   return (
-    <div className='mywork'>
+    <div id='work' className='mywork'>
         <div className='mywork-title'>
         <h1>My latest work</h1>
         <img src={theme_pattern} alt="" />
